10784a41e9a28c159f03fde77ea0e98dd1ed175b
Stefan Maciolek <51088087+Smaciolek94@users.noreply.github.com>
2020-04-21 17:18:32 -0400

    4/21 updates

--- Rcpp_source.cpp
+++ Rcpp_source.cpp
@@ -12,23 +12,24 @@ using namespace Rcpp;
 //
 
 // [[Rcpp::export]]
-NumericVector first(NumericMatrix uscases, int row){
+int first(NumericMatrix uscases, int row){
   int n = uscases.ncol();
   NumericVector temp(n);
-  //for (int i=0; i < n; ++i) {
-  //  NumericVector temp(n);
-    //temp = uscases[Range(0,n),row];
-  //  temp[i]= uscases(i,row);
- // }
- temp = uscases(row,_);
- NumericVector date(n);
- date[0] = 1;
- for (int i=0; i<n; ++i){
-   date[i] += i;
- }
- //NumericMatrix tempcases(2,n);
- //tempcases(1,_) = date;
- //tempcases(2,_) = temp;
-  return temp;
+  temp = uscases(row,_);
+  NumericVector date(n);
+  date[0] = 1;
+  for (int i=0; i<n; ++i){
+    date[i] += i;
+  }
+  int first = 0;
+  for (int i =0;i<n;++i){
+    if (temp[i] != 0){
+      first = date(i);
+     // return first;
+    }
+    if (temp[i] != 0){break;}
+  }
+  //int len = first.length();
+  return first;
 }
 
--- first date Rcpp.R
+++ first date Rcpp.R
@@ -1,10 +1,7 @@
 uscases <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"))
 usdeaths <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"))
-#uscases <- uscases[,-c(1:10)]
-#usdeaths <- usdeaths[,-c(1:10)] #use these for the R code
-
-uscases <- uscases[,-c(1:11)]
-usdeaths <- usdeaths[,-c(1:12)] #use these for the c++ code
+uscases <- uscases[,-c(1:10)]
+usdeaths <- usdeaths[,-c(1:10)] #use these for the R code
 
 n <- ncol(uscases) - 1
 date <- 1:n
@@ -23,8 +20,44 @@ first <- as.Date(first,origin = "1970-01-01")
 first <- data.frame(uscases$Combined_Key,first)
 hist(first$first,breaks=n,main="Histogram of the date of first cases in the US",ylab="number of counties/cities",xlab="Date of First Case",freq=TRUE)
 
-casemat <- as.matrix(uscases)
-dim(casemat) <- c(3255,87)
+########################################################################################
+####################################################################################################
+################################################################################################################
 
+uscases <- uscases[,-c(1)]
+usdeaths <- usdeaths[,-c(1,2)] #use these for the c++ code
+casemat <- as.matrix(uscases)
+
+n<-ncol(uscases)
 #trying to make a c++ funtion to speed it up
 library(Rcpp)
+
+cppFunction('int first(NumericMatrix uscases, int row){
+  int n = uscases.ncol();
+  NumericVector temp(n);
+  temp = uscases(row,_);
+  NumericVector date(n);
+  date[0] = 1;
+  for (int i=0; i<n; ++i){
+    date[i] += i;
+  }
+  int first = 0;
+  for (int i =0;i<n;++i){
+    if (temp[i] != 0){
+      first = date(i);
+      // return first;
+    }
+    if (temp[i] != 0){break;}
+  }
+  //int len = first.length();
+  return first;
+}')
+
+firstdate <- rep(0,nrow(uscases))
+for(i in 1:nrow(uscases)-1){
+  firstdate[i] <- first(casemat,i)
+}
+firstdate <- firstdate[which(firstdate!=0)]
+firstdate <- as.Date(firstdate,origin = "2020-01-22")
+#first <- data.frame(uscases$Combined_Key,first)
+hist(firstdate,breaks=n,main="Histogram of the date of first cases in the US",ylab="number of counties/cities",xlab="Date of First Case",freq=TRUE)
