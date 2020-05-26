ccc81ea7f4405e714438457244cfd8027dc45e3e
Stefan Maciolek <51088087+Smaciolek94@users.noreply.github.com>
2020-05-26 10:48:47 -0400

    Update Time Series.R

---
+++ Time Series.R
@@ -0,0 +1,8 @@
+uscases <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"))
+usdeaths <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"))
+uscases <- uscases[,-c(1:5,8:11)]
+usdeaths <- usdeaths[,-c(1:5,8:12)]
+n <- ncol(uscases)-2
+date <- 1:n
+date <- as.Date(date,origin = "2020-01-21")
+format(date,format = "%b %d %y")
