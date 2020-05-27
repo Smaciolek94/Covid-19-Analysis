23a7da37c3d428c6f1cd5453345ecdc43a105f00
Stefan Maciolek <51088087+Smaciolek94@users.noreply.github.com>
2020-05-27 15:19:37 -0400

    5/27

--- Time Series.R
+++ Time Series.R
@@ -11,7 +11,7 @@ library(TSA)
 
 names <- unique(uscases$Province_State)
 
-#total cases:
+#this function creates a time series of the selected state
 cases <- function(state){
   totcase <- colSums(uscases[which(uscases$Province_State == state),3:(n+2)])
   newcase <- rep(0,n)
@@ -21,8 +21,41 @@ cases <- function(state){
   return(newcase)
 }
 
+#test
 plot(cases("New York"))
+
+#this function performs EDA on the state time series to inform model building
+prelim <- function(state){
+  series <- cases(state)
+  dif <- diff(series)
+  weekdif <- diff(series,7)
+  plot(series,main="undifferenced time series")
+  plot(dif,main="first difference of time series")
+  plot(weekdif,main="first weekly difference of time series")
+  acf(as.vector(series),main=paste(state,"ACF"))
+  pacf(as.vector(series),main=paste(state,"PACF"))
+  acf(as.vector(dif),main=paste(state,"1st difference ACF"))
+  pacf(as.vector(dif),main=paste(state,"1st difference PACF"))
+  acf(as.vector(weekdif),main=paste(state,"1st weekly difference ACF"))
+  pacf(as.vector(weekdif),main=paste(state,"1st weekly difference PACF"))
+  periodogram(series,main="Periodogram of time series")
+}
+prelim("New York")
+
+model<-arima(cases(state),order=c(1,0,0),seasonal=list(order=c(0,0,0),period=7))
+coefupper <- model$coef + diag(sqrt(model$sigma2))
+coeflower <- model$coef - diag(sqrt(model$sigma2))
+model("New York")
 
 time <- sapply(names,cases)
 
-model<-arima(cases("New York"),order=c(1,0,0),seasonal=list(order=c(1,0,1),period=7))
+model<-arima(cases("New York"),order=c(1,1,1),seasonal=list(order=c(0,0,0),period=7))
+
+diag<-function(model){
+  resid <- model$residuals
+  plot(resid,main="Final Model Residuals")
+  qqnorm(resid,main="QQplot of Residuals")
+  hist(resid,main="Histogram of Residuals")
+  acf(as.vector(resid),main="ACF of Residuals")
+}
+diag(model)
